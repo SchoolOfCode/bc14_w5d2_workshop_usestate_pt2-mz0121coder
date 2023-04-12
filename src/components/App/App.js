ec0ff395fe5010a8b4837414c53aef9fc09b339f
Mohammad Zafar <mzcoder7@gmail.com>
2023-04-12 16:17:28 +0100

    task 1 and 2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
+import Input from "../Input/index.js";
+import Item from "../Item/index.js";
+/* NOTES FOR REFERENCE 
+In the App component, text state and handleChange function are passed as props to Input component using text and handleChange attributes. text state is also passed as a prop to Item component using text attribute.
 
+In the Input component, text and handleChange are received as props and are used to set the value and onChange attributes of the input element respectively.
+
+In the Item component, text is received as a prop and is used to display the text inside the li element.*/
 function App() {
+  const [text, setText] = useState("");
+  function handleChange(event) {
+    setText(event.target.value);
+  }
   return (
     <div className="App">
       <h1>JSX will go here!</h1>
+      <Input text={text} handleChange={handleChange} />
+      <Item text={text} />
     </div>
   );
 }
